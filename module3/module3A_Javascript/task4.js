26fd796784ba74837797dfc54de7d69c01422a5b
UDEME JALEKUN <basseyudeme37@yahoo.com>
2023-07-16 13:53:46 +0100

    add task4 and 16

---
+++ module3/module3A_Javascript/task4.js
@@ -0,0 +1,3 @@
+// //Lesson 8
+// alert("Hello, world!") // this code can be added to an index.html file which can be viewed on live server
+console.log("Hello, world!") // this logs the comment on the terminal and is not an alert that pops up on browser
