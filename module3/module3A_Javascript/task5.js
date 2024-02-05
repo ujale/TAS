ca9a067a2b75a2647a16b81969eca9e6fb16c7ab
Udeme Jalekun <47925189+ujale@users.noreply.github.com>
2024-02-05 13:24:27 +0100

    Update task5.js

--- module3/module3A_Javascript/task5.js
+++ module3/module3A_Javascript/task5.js
@@ -1,4 +1,19 @@
-//Module 2, task 5
+//Task 5
+/* 1. Create a variable named day and Assign it a the current day of the week as string (eg,
+Thursday) 
+
+
+2. Create another variable name position and assign it a number which represents the
+position of the current day in days of the week. (eg, if today is Thursday, position should
+be assigned 4) 
+
+
+3. Create another variable named output and assign it a string that reads:
+Today is Thursday. It is the 4th day of the week. (Use string concatenation to add the
+variables declared in steps and 2 above)
+
+*/
+
 const day = "Monday"
 const position = 1
 const output = 'Today is ' + day + '.' + ' It is the ' + position + 'st day of the week.'
