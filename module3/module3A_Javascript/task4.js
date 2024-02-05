ce964c85b738bae2bb30537e5ccaeeab8106ca30
Udeme Jalekun <47925189+ujale@users.noreply.github.com>
2024-02-05 11:19:07 +0100

    Update task4.js

--- module3/module3A_Javascript/task4.js
+++ module3/module3A_Javascript/task4.js
@@ -1,3 +1,5 @@
 // //Lesson 8
+//  write a JavaScript program that shows the text, Hello, world! As an alert on the browser.
+
 // alert("Hello, world!") // this code can be added to an index.html file which can be viewed on live server
 console.log("Hello, world!") // this logs the comment on the terminal and is not an alert that pops up on browser
